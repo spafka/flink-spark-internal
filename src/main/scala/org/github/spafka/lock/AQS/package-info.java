d530c6954451e328ea531bcacc831e0886737672
wanghf <wanghf@iovdc.cn>
2018-10-30 18:20:34 +0800

    add future demo

--- src/main/scala/org/github/spafka/lock/AQS/package-info.java
+++ src/main/scala/org/github/spafka/lock/AQS/package-info.java
@@ -1,3 +1,15 @@
 package org.github.spafka.lock.AQS;
 
-//@see https://www.cnblogs.com/chengxiao/archive/2017/07/24/7141160.html
+//@see https://www.cnblogs.com/chengxiao/archive/2017/07/24/7141160.html\
+
+
+/**
+ * CLH 队列存放线程,如果没获取到锁，即state状态一般不为0，则把此线程中断 park，并加入到CLH中，直到其前置 head node 任务完成，然后由前置线程任务unlock，
+ * release 释放tail node，unpark之
+ *
+ * <pre>
+ *      +------+  prev +-----+       +-----+
+ * head |      | <---- |     | <---- |     |  tail
+ *      +------+       +-----+       +-----+
+ * </pre>
+ */
